69193c517ff679c6f3360f2cee7b2c2233c90501
Veske <veske@tlu.ee>
2014-10-11 23:47:17 +0300

    Allow uploading media files:
    * Corrections to index page.

--- app/models/media.rb
+++ app/models/media.rb
@@ -2,8 +2,8 @@ class Media < ActiveRecord::Base
 	# Check presence of medium file is true. Set keys for 3 different sizes of the medium.
 	# Check that the file type is accepted by the array provided by us
 	# Check that file is less than 1MB
-	has_attached_file :image_video, :styles => {:thumb => 'x100', :croppable => '600x600>', :big => '1000x1000>'}
-	validates_attachment_content_type :image_video, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+	has_attached_file :image_video, :styles =>  {:thumb => '200x250>', :croppable => '600x600>', :big => '1000x1000>'}
+	validates_attachment_content_type :image_video, :content_type => ['image/jpg', 'image/jpeg', 'image/png', 'image/gif']
 	validates_with AttachmentSizeValidator, :attributes => :image_video, :less_than => 1.megabytes
 
 	belongs_to :user
