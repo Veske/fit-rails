0e633a8e1c8ca107c0913db31b86f2eba6a9e7e5
Veske <veske@tlu.ee>
2014-10-17 00:24:50 +0300

    Allow-making-comments:   * Implemented checks for destroy method on comments

--- app/assets/javascripts/comments.coffee
+++ app/assets/javascripts/comments.coffee
@@ -3,23 +3,17 @@
 # You can use CoffeeScript in this file: http://coffeescript.org/
 
 $(document).on 'ready page:load', ->
-  console.log('Ready')
+
+  # Seems that doing this by just using CScript will be troublesome. Wait for AngularJS implementation
 
   $('form').submit ->
-    $.ajax '/comments/create',
+    $.ajax '/comments',
       type: 'GET'
+      data:  { media_id: 1 }
       dataType: "JSON"
       error: (jqXHR, textStatus, errorThrown) ->
         $('h2').append "AJAX Error: #{textStatus}"
       success: (data, textStatus, jqXHR) ->
+        for f in data
+          console.log(f.text)
         $('h2').append "Successful AJAX call: #{data}"
-
-
-
- # $.ajax '/',
-  #  type: 'GET'
-   # dataType: 'json'
-   # error: (jqXHR, textStatus, errorThrown) ->
-   #   $('body').append "AJAX Error: #{textStatus}"
-   # success: (data, textStatus, jqXHR) ->
-    #  $('body').append "Successful AJAX call: #{data}"
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,10 +2,8 @@ class CommentsController < ApplicationController
 
 	def index
 		@comments = Comment.all
-	end
-
-	def show
-
+		render :json => Comment.where(media_id: params[:media_id])
+		#redirect_to medium_path(params[:media_id]), alert: 'Your comment was created!'
 	end
 
 	def create
@@ -13,19 +11,21 @@ class CommentsController < ApplicationController
 
 		if @comment.save
 			logger.info "INFO: New post created with id #{@comment.id}."
-			render json: Comment.find_by(media_id: params[:comment][:media_id])
+			render :nothing => true
 		else
 			logger.debug "ERROR: failed to create new comment #{@comment.errors.full_messages}."
+			render :nothing => true
 		end
 	end
 
 	def destroy
-		@comment = Comment.find(params[:id]).destroy
+		@comment = Comment.find(params[:id])
 
-		if @comment.destroy
+		if  current_user.id == @comment.user_id && @comment.destroy
 			logger.info "INFO: Comment with id: #{@comment.id} removed."
 			redirect_to medium_path(@comment.media_id), notice: 'Comment deleted!'
 		else
+			# TODO: add validation to Comment for this case
 			logger.debug "ERROR: failed to remove comment with id #{@comment.id}, Thrown errors: #{@comment.errors.full_messages}."
 			redirect_to medium_path(@comment.media_id), alert: "Couldn't delete comment: #{@comment.errors.full_messages}."
 		end
