908a909f0503df952e03d3430ec6839871363d58
Veske <veske@tlu.ee>
2014-10-19 19:20:02 +0300

    Allow commenting media:   * Enable glyphicons

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -5,10 +5,11 @@ class Medium < ActiveRecord::Base
 	# Check presence of medium file is true. Set keys for 3 different sizes of the medium.
 	# Check that the file type is accepted by the array provided by us
 	# Check that file is less than 1MB
+	validates :image_video, presence:   { message: 'No file was chosen for upload'}
 	has_attached_file :image_video,
-		:styles =>  {:thumb => '200x250>', :croppable => '600x600>', :big => '1000x1000>'}
+		styles:     {thumb: '200x250>', croppable: '600x600>', big: '1000x1000>'}
 	validates_attachment_content_type :image_video, :content_type => %w(image/jpeg image/jpg image/png image/gif)
-	validates_with AttachmentSizeValidator, :attributes => :image_video, :less_than => 1.megabytes
+	validates_with AttachmentSizeValidator, attributes: :image_video, less_than: 1.megabytes
 
 	private
 		def randomize_file_name
