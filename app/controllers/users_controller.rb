7aeaa8016919daedfbe7292e60dc3d37f5089dc6
Veske <veske@tlu.ee>
2014-10-20 20:02:43 +0300

    Genereic fixes:    * User can see all their uploaded media on their profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
 
 	def show
 		user = User.find(params[:id])
-		locals user: user
+		locals user: user, media: Medium.where(user_id: user.id)
 	end
 
 	# TODO: This action was here by default, but the whole process seems to work without it too.
